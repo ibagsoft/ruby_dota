fbca7c3561999d230bb717f035e6790566361c2c
ibagsoft <ibagsoft@gmail.com>
2013-04-16 23:25:31 +0800

    指定到site.url

--- js/post.js
+++ js/post.js
@@ -101,7 +101,7 @@ $(document).ready(function(){
         }
     }
 
-    $.getScript('/js/prettify/prettify.js',function(){
+    $.getScript('{{site.url}}/js/prettify/prettify.js',function(){
         prettyPrint();
         menuIndex();
     });
